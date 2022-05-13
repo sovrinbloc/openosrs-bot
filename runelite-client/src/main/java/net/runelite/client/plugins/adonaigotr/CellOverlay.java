88550b6fb7c3be0771768cac082f66eb19db7670
josephalai@gmail.com <josephalai@gmail.com>
2022-05-13 12:51:14 -0700

    removes gotr plugin

--- runelite-client/src/main/java/net/runelite/client/plugins/adonaigotr/CellOverlay.java
+++
@@ -1,2 +0,0 @@
-package net.runelite.client.plugins.adonaigotr;public class CellOverlay {
-}
--- runelite-client/src/main/java/net/runelite/client/plugins/adonaigotr/GuardiansOfTheRiftPlugin.java
+++
@@ -1,8 +0,0 @@
-package net.runelite.client.plugins.adonaigotr;
-
-import net.runelite.client.plugins.Plugin;
-
-public class AdonaiGOTRPlugin extends Plugin
-{
-
-}
